3d17332cdb48726ccea1aa59ee15739dfe6ca47a
sheldonu <uch20002@byui.edu>
2024-05-06 16:00:19 -0700

    MVC debug

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/index")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates

77cb9a3881146328b60de7b2eef1d6fc23267add
sheldonu <uch20002@byui.edu>
2024-05-06 21:05:42 -0700

    model inventory changed

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -31,10 +31,8 @@ async function getInventoryByClassificationId(classification_id) {
 async function getDataByInventoryId(inventory_id) {
   try {
     const data = await pool.query(
-      `SELECT * FROM public.inventory AS i 
-      JOIN public.classification AS c 
-      ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1`,
+      `SELECT * FROM public.inventory
+      WHERE inv_id = $1`,
       [inventory_id]
     )
     return data

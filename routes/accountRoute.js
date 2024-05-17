2cc4b08e62e55956ab438aee9b2b8b9e4b5c8898
sheldonu <uch20002@byui.edu>
2024-05-17 11:21:48 -0700

    forms and validation added

---
+++ routes/accountRoute.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = new express.Router()
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
+
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+router.post("/register", regValidate.registationRules(),
+regValidate.checkRegData,
+utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router
--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * View Engine and Templates
@@ -20,6 +23,29 @@ const utilities = require("./utilities/")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * Routes
@@ -30,6 +56,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Account routes
+app.use("/account", require("./routes/accountRoute"))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
